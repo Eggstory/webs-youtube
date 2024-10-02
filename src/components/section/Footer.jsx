eaf6f4365961650d12b4c60d3e82dfc79d24ec75
Eggstory <khm9963@gmail.com>
2024-10-02 13:06:17 +0900

    메인 콘텐츠 작업

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer id="footer" role="contentinfo">
-      Footer
+      <a href="mailto:khm9963@naver.com" rel='noopenner noreferrer'>
+        khm9963@naver.com
+      </a>
     </footer>
   );
 };
